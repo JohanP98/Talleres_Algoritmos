da3df613fcb1cbb5af09b2d9580b13f7225016e3
Johan Pinzón <114430943+JohanP98@users.noreply.github.com>
2022-10-03 12:42:32 -0500

    cambio

---
+++ Estructuras de Control Secuenciales/Hola_Mundo.py
@@ -0,0 +1,9 @@
+print("Hola Mundo!!")
+#entradas sola linea
+edad_uno=int(input("ingrese edad 1: "))
+edad_dos=int(input("ingrese edad 2: "))
+edad_tres=int(input("ingrese edad 3: "))
+#caja negra
+promedio=(edad_uno+edad_dos+edad_tres)/3
+#salidas
+print("El promedio de las edades es:", promedio)
