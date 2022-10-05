c4db4d4b2065b4a5ccd3e7527ef02a715a03f48c
Johan Pinzón <114430943+JohanP98@users.noreply.github.com>
2022-10-05 09:50:07 -0500

    cambios

---
+++ Estructuras de Control Secuenciales/ejercicio 2.py
@@ -0,0 +1,10 @@
+#entradas
+import math
+
+capital=float(input("Ingrese el capital que invirtió:"))
+
+#caja negra
+gananciasmes=(capital*(2/100))
+
+#salidas
+print("las ganancias despues de un mes de su inversión sera de:", gananciasmes,"pesos") 
