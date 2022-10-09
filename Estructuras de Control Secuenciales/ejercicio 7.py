8454c6fa33559033d280f3b0a3ef853dbb208bb0
Johan Pinzón <114430943+JohanP98@users.noreply.github.com>
2022-10-09 14:58:39 -0500

    ejercicio 7

---
+++ Estructuras de Control Secuenciales/ejercicio 7.py
@@ -0,0 +1,12 @@
+#entradas
+import math
+
+Metros=int(input("Escriba el valor en metros:"))
+
+#caja negra
+pulgadas=(Metros*39.27)
+píes=(pulgadas/12)
+
+#salidas
+print("El valor en pulgadas de los metros dados es de:", pulgadas,"pulgadas")
+print("El valor en pies de los metros dados es de:", píes,"píes")
