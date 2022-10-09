c5605daf0796652aa14e6496d693118d91c8b4e4
Johan Pinzón <114430943+JohanP98@users.noreply.github.com>
2022-10-09 18:56:00 -0500

    Update ejercicio 14.py

--- Estructuras de Control Secuenciales/ejercicio 14.py
+++ Estructuras de Control Secuenciales/ejercicio 14.py
@@ -4,11 +4,8 @@ import math
 lecturaanterior=int(input("Escriba el monto de la lectura electrica anterior:"))
 lecturaactual=int(input("Escriba el monto de la lectura electrica actual:"))
 kilovatio=int(input("Escriba el valor del kilovatio:"))
-montoanterior=int(input("Escriba el monto que se pagó en el mes anterior"))
 
 #Caja negra
-montoadicional=abs(lecturaanterior-lecturaactual)*kilovatio
-montofinal=(lecturaactual*kilovatio)
+montofinal=abs(lecturaactual-lecturaanterior)*kilovatio
 #Salidas
-print("El monto que se adiciona al monto del mes anterior es de:",montoadicional,"COP")
 print("El monto final a pagar será de:",montofinal,"COP")
