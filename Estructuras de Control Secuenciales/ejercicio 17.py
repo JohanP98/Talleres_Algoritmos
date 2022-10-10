3505d53e7364e32c0f21a904bb5c66e49b2f653c
Johan Pinzón <114430943+JohanP98@users.noreply.github.com>
2022-10-09 22:39:21 -0500

    Create ejercicio 17.py

---
+++ Estructuras de Control Secuenciales/ejercicio 17.py
@@ -0,0 +1,14 @@
+#Entradas
+import math
+
+monto=int(input("Escriba el valor del monto para el presupuesto anual:"))
+
+#Caja negra
+Ginecologia=(monto*0.40)
+Traumatologia=(monto*0.30)
+Pediatria=(monto*0.30)
+
+#Salidas
+print("El dinero que va a recibir el area de Ginecología es de:",Ginecologia,"COP")
+print("El dinero que va a recibir el area de Traumatología es de:",Traumatologia,"COP")
+print("El dinero que va a recibir el area de Pediatría es de:",Pediatria,"COP")
