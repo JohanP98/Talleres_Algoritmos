e38efde31264d323203ae0abeb482223d13e9533
Johan Pinzón <114430943+JohanP98@users.noreply.github.com>
2022-10-09 22:18:22 -0500

    Create ejercicio 16.py

---
+++ Estructuras de Control Secuenciales/ejercicio 16.py
@@ -0,0 +1,11 @@
+#Entradas
+import math
+
+ngalones=float(input("Escriba el numero de galones que le puso o le pondra a su carro:"))
+
+#Caja negra
+valorgalón=(50000*3.785)
+valorngalones=(ngalones*valorgalón)
+
+#Salida
+print(ngalones,"galones le costaria un total de:",valorngalones,"COP")
